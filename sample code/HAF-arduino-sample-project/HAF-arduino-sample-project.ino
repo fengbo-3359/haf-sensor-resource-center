d5f07bf58dd3c5f3101d5fad3e1e433754320b44
fengbo <809223299@qq.com>
2022-03-14 16:07:51 +0800

    init

---
+++ sample code/HAF-arduino-sample-project/HAF-arduino-sample-project.ino
@@ -0,0 +1,133 @@
+#include "HAF.h"
+#include "Port.h"
+
+HafSensorConfig senssor = {0};
+void setup()
+{
+  // put your setup code here, to run once:
+  senssor.i2cAddress = 0x48;
+  senssor.flowOffset = 0;
+  Port_I2cInit();
+  Serial.begin(9600);
+  Serial.print("I2c addr: 0x");
+  Serial.println(senssor.i2cAddress, HEX);
+  //suggest warm up time
+  delay(50);
+
+  /*********************get serial number**********************/
+  while (HAF_GetSn(senssor.i2cAddress, senssor.sn) != 0)
+  {
+    Serial.println("Get sn error!");
+    delay(1000);
+  }
+  Serial.print("SN: ");
+  Serial.print(senssor.sn[0] < 16 ? "0" : "");
+  Serial.print(senssor.sn[0], HEX);
+  Serial.print(senssor.sn[1] < 16 ? "0" : "");
+  Serial.print(senssor.sn[1], HEX);
+  Serial.print(senssor.sn[2] < 16 ? "0" : "");
+  Serial.print(senssor.sn[2], HEX);
+  Serial.print(senssor.sn[3] < 16 ? "0" : "");
+  Serial.println(senssor.sn[3], HEX);
+
+  /************************get factor*************************/
+  while (HAF_GetFactor(senssor.i2cAddress, &senssor.flowFactor) != 0)
+  {
+    Serial.println("Get factor error!");
+  }
+  Serial.print("Factor: ");
+  Serial.println(senssor.flowFactor, DEC);
+
+  //get unit
+  if (HAF_GetUnit(senssor.i2cAddress, &senssor.flowUnit) != 0)
+  {
+    Serial.println("Get unit error!");
+  }
+  else
+  {
+    Serial.print("Unit: ");
+    if (senssor.flowUnit == HAF_UNIT_SCCM)
+      Serial.println("sccm");
+    else if (senssor.flowUnit == HAF_UNIT_SLM)
+      Serial.println("slm");
+  }
+
+  /**************************set gas type**********************/
+  if (HAF_SetGasType(senssor.i2cAddress, HAF_GAS_TYPE_AIR) != 0)
+  {
+    Serial.println("Set gas type error!");
+  }
+  else
+  {
+    Serial.println("Set gas type successfully!");
+  }
+
+  /*********************set gas proportion**********************/
+  if (HAF_SetGasProportion(senssor.i2cAddress, 1000) != 0)
+  {
+    Serial.println("Set gas proportion error!");
+  }
+  else
+  {
+    Serial.println("Set gas proportion successfully!");
+  }
+
+  /*********************get gas type***************************/
+  if (HAF_GetGasType(senssor.i2cAddress, &senssor.gasType) != 0)
+  {
+    Serial.println("Get gas type error!");
+  }
+  else
+  {
+    Serial.print("Gas type: ");
+    if (senssor.gasType == HAF_GAS_TYPE_AIR)
+      Serial.println("Air");
+    else if (senssor.gasType == HAF_GAS_TYPE_O2)
+      Serial.println("O2");
+    else if (senssor.gasType == HAF_GAS_TYPE_N2O)
+      Serial.println("N2O");
+    else if (senssor.gasType == HAF_GAS_TYPE_O2_AIR)
+      Serial.println("O2&Air");
+    else if (senssor.gasType == HAF_GAS_TYPE_N2O_AIR)
+      Serial.println("N2O&Air");
+    else if (senssor.gasType == HAF_GAS_TYPE_O2_N2O)
+      Serial.println("O2&N2O");
+  }
+
+  /*********************get gas proportion**********************/
+  if (HAF_GetGasProportion(senssor.i2cAddress, &senssor.gasProportion) != 0)
+  {
+    Serial.println("Get gas proportion error!");
+  }
+  else
+  {
+    Serial.print("Gas proportion: ");
+    Serial.print(senssor.gasProportion, DEC);
+    Serial.println("‰");
+  }
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  /*********************get flow and temp**********************/
+  if (HAF_GetFlowTemp(senssor.i2cAddress, &senssor.flow, &senssor.temp) != 0)
+  {
+    Serial.println("Get flow and temp error!");
+  }
+  else
+  {
+    Serial.print("Flow: ");
+    Serial.print((float)senssor.flow / senssor.flowFactor, 3);
+    if (senssor.flowUnit == HAF_UNIT_SCCM)
+      Serial.print("sccm");
+    else if (senssor.flowUnit == HAF_UNIT_SLM)
+      Serial.print("slm");
+    Serial.print("   Temp: ");
+    Serial.print((float)senssor.temp / 100, 1);
+    Serial.println("℃");
+  }
+
+  delay(1000);
+
+}
---
+++ sample code/HAF-arduino-sample-project/Port.cpp
@@ -0,0 +1,72 @@
+/*
+   Copyright (c) 2021-2022, Honeywell Air Flow Team
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ * * Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+ * * Neither the name of Honeywell Air Flow Team nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+
+   Change Logs:
+   Date           Author       Notes
+   2022-03-1     fengbo      the first version
+
+*/
+#include <Arduino.h>
+#include <Wire.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Port_I2cInit(void)
+{
+  Wire.begin();
+}
+
+int Port_I2cRead(byte address, byte *data, word count)
+{
+  byte readData[count];
+  word rxByteCount = 0;
+
+  Wire.requestFrom(address, count);
+  while (Wire.available())
+  { // wait till all arrive
+    readData[rxByteCount++] = Wire.read();
+    if (rxByteCount >= count)
+      break;
+  }
+  memcpy(data, readData, count);
+  return 0;
+}
+
+int Port_I2cWrite(byte address, const byte *data, word count)
+{
+  Wire.beginTransmission(address);
+  Wire.write(data, count);
+  Wire.endTransmission();
+  return 0;
+}
+#ifdef __cplusplus
+}  // extern "C"
+#endif
